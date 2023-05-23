07b738ba8a5e21aeebff27679c14f73f82ee61b9
arbobm <brunameneses@gmail.com>
2023-05-23 17:24:25 -0300

    adiciona funcao reactable

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,8 @@
 # # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
-# 
+# # 
 # pacotes <- c("rhino", "leaflet", "shinyWidgets", "sf", "raster", "dplyr",
-#              "stringr", "htmltools", "readxl", "shinybusy", "leaflet.extras")
+#              "stringr", "htmltools", "readxl", "shinybusy", "leaflet.extras",
+#              "reactable", "janitor", "lubridate")
 # 
 # # In R console
 # renv::install(pacotes)
@@ -20,6 +21,9 @@ library(htmltools)
 library(readxl)
 library(shinybusy)
 library(leaflet.extras)
+library(reactable)
+library(janitor)
+library(lubridate)
 
 ## in R console
-renv::snapshot()
+# renv::snapshot()
